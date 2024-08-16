8a36d613f3c360575a8a245907733e3d71f65d24
Hilmar Wiegand <me@hwgnd.de>
2024-08-16 12:29:44 +0200

    Add lobby element and implement match joining

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Client } from "boardgame.io/client"
+import { Client, LobbyClient } from "boardgame.io/client"
 import { Local, SocketIO } from "boardgame.io/multiplayer"
 import { onClick, resetOnClicks, ctx } from "./canvas"
 import { Debug } from "boardgame.io/debug"
@@ -9,12 +9,14 @@ const isMultiplayer = import.meta.env.VITE_REMOTE === "true"
 const multiplayerServer =
   import.meta.env.VITE_MUTLIPLAYER_SERVER ?? "localhost:8000"
 
+const lobbyClient = new LobbyClient({ server: "http://localhost:8000" })
+
 const multiplayer = isMultiplayer
   ? SocketIO({ server: multiplayerServer })
   : Local()
 
 class GameClient {
-  constructor(rootElement) {
+  constructor(rootElement, matchId) {
     this.rootElement = rootElement
 
     this.client = Client({
@@ -25,6 +27,7 @@ class GameClient {
         hideToggleButton: false,
         impl: Debug,
       },
+      matchID: matchId,
     })
 
     this.client.subscribe((state) => render(state, ctx, resetOnClicks, onClick))
@@ -33,4 +36,31 @@ class GameClient {
 }
 
 const appElement = document.getElementById("app")
-new GameClient(appElement)
+const lobbyElement = document.getElementById("lobby")
+const matchIdElement = document.getElementById("match-id")
+
+if (!isMultiplayer) {
+  document.getElementById("app").classList.remove("hidden")
+  new GameClient(appElement)
+} else {
+  document.getElementById("lobby").classList.remove("hidden")
+
+  document.getElementById("create-game").addEventListener("click", async () => {
+    const result = await lobbyClient.createMatch("default", {
+      numPlayers: 4,
+    })
+
+    new GameClient(appElement, result.matchID)
+    matchIdElement.classList.remove("hidden")
+    matchIdElement.innerText = result.matchID
+
+    lobbyElement.classList.add("hidden")
+    appElement.classList.remove("hidden")
+  })
+
+  document.getElementById("join-game").addEventListener("click", async () => {
+    const gameID = document.getElementById("join-match-id").value
+    if (!gameID) return
+    console.log(`joining game ${gameID}`)
+  })
+}
