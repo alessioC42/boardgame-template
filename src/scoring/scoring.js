bf3da21b08d54a0d4a47883f59d4e77efdd8d304
DESKTOP-OFGSGSD\TN <jan+4@opencreek.tech>
2024-08-19 10:31:21 +0200

    wip: implement diffenrent victory conditions

--- src/scoring/scoring.js
+++ src/scoring/scoring.js
@@ -5,7 +5,7 @@ import {
 } from "../cascadia"
 export function scoring() {}
 
-function getNeighbourAnimals(board, coordinates) {
+export function getNeighbourAnimals(board, coordinates) {
   let neighbourCoordinates = getNeighbourCoordinates(coordinates)
   let animals = []
   for (let nC of neighbourCoordinates) {
@@ -15,4 +15,37 @@ function getNeighbourAnimals(board, coordinates) {
   }
 
   return animals
+}
+
+export function getNeighbourAnimalsOfType(board, coordinates, displayName) {
+  let result = []
+  let neighbourCoordinates = getNeighbourCoordinates(coordinates)
+  for (let nC of neighbourCoordinates) {
+    const [x, y] = nC
+    if (board[x][y].displayName == displayName) {
+      result.push(coordinates)
+    }
+  }
+  return result
+}
+
+export function countAnimalList(list) {
+  let counts = {}
+  for (let animal of list) {
+    if (counts[animal.displayName] == undefined) {
+      counts[animal.displayName] = 0
+    }
+    counts[animal.displayName] += 1
+  }
+  return counts
+}
+
+export function addCoordsToListIfNotAlreadyInList(list, coords) {
+  for (let item of list) {
+    if (item[0] == coords[0] && item[1] == item[1]) {
+      return
+    }
+  }
+  list.push(coords)
+  return list
 }
